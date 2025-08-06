511d9f5b4b4958252690966f3b7c4c62159f4ac9
Gurpreet <ajbagdi6@gmail.com>
2025-08-06 23:44:30 +0530

    added btn and form validation

--- src/Pages/FormValidation.jsx
+++ src/Pages/FormValidation.jsx
@@ -1,32 +1,32 @@
 export default function FormValidation() {
   return (
-    <div className="bg-[#171717] text-[#4F4F4F] h-[40vh]">
+    <div className="bg-[#171717] text-[#4F4F4F] h-[43vh]">
       <h1 className="text-center text-white font-bold ">Get in touch</h1>
       {/* Form div  */}
       <div className="flex flex-col justify-center ">
-        <div className="flex justify-center items-start h-full p-5 pt- ">
+        <div className="flex justify-center items-start h-full p-5 ">
           <div className=" text-center">
             <input
               type="text"
               placeholder="Name"
-              className="border-b-2 placeholder-[#4F4F4F] w-30 text-[13px] pt-5 h-10"
+              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
             />
             <input
               type="text"
               placeholder="Last name"
-              className="border-b-2 placeholder-[#4F4F4F] w-30 text-[13px] pt-5 h-10"
+              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
             />
           </div>
           <div className="text-center">
             <input
               type="text"
               placeholder="email"
-              className="border-b-2 placeholder-[#4F4F4F] w-30 text-[13px] pt-5 h-10"
+              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
             />
             <input
               type="text"
               placeholder="Phone"
-              className="border-b-2 placeholder-[#4F4F4F] w-30 text-[13px] pt-5 h-10"
+              className="border-b-2  border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
             />
           </div>
         </div>
@@ -34,8 +34,9 @@ export default function FormValidation() {
           name="msg"
           placeholder="Massege here .. "
           id=""
-          className="placeholder-[#4F4F4F] border h-30 w-90 ml-6"
+          className="placeholder-[#4F4F4F] border h-30 w-90 ml-3 p-2 text-white "
         ></textarea>
+        <button className=" text-white bg-green-600 text-[12px] md:text-lg border-white border-1 m-auto mt-2 h-6 p-1">Submit Here</button>
       </div>
     </div>
   );
--- src/Pages/WhatDo.jsx
+++ src/Pages/WhatDo.jsx
@@ -4,12 +4,12 @@ export default function WhatDo() {
   return (
     <>
       <div className="  bg-[#171717] md:pt-10 h-50 md:h-[100vh] md:items-start   flex flex-col justify-start">
-        <h1 className=" bg-[#171717] md:text-4xl  font-bold text-center text-white ">
+        <h1 className=" bg-[#171717] md:text-4xl mt-5  font-bold text-center text-white ">
           What i Do
         </h1>
 
         {/* Four divs */}
-        <div className="flex  gap-5 justify-center pt-3">
+        <div className="flex  gap-5 justify-center pt-4">
           <div className={border}>
             <i className="fa-solid fa-laptop-code  text-green-400  md:text-[50px]"></i>
             <h1 className="text-[9px] md:text-2xl md:pt-5 text-[#535353]">
