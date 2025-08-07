ad92c0c56c01d0b0e6de5fc9e584b8810e1cc7c3
Gurpreet <ajbagdi6@gmail.com>
2025-08-08 03:15:56 +0530

    changed

--- src/Pages/FormValidation.jsx
+++ src/Pages/FormValidation.jsx
@@ -13,17 +13,14 @@ export default function FormValidation() {
 
   let submitForm = (data) => {
     console.log(data);
-    setuserData([{ ...userdata },data]);
+    setuserData([{ ...userdata }, data]);
 
     console.log(userdata);
     reset();
   };
-  useEffect(() => {
-    setuserData([{ ...userdata }]);
-  }, []);
 
   return (
-    <div className="bg-[#171717] text-[#4F4F4F] h-[50vh]">
+    <div className="bg-[#171717] text-[#4F4F4F] h-[60vh]">
       <h1 className="text-center text-white font-bold ">Get in touch</h1>
       {/* Form div  */}
 
@@ -31,45 +28,54 @@ export default function FormValidation() {
         onSubmit={handleSubmit(submitForm)}
         className=" flex justify-center items-start h-full p-5 "
       >
-        <div className="flex flex-col ">
-          <div className=" text-center">
+        <div className="flex flex-col">
+          <div className=" text-center flex flex-col items-center h-30 py-2 relative">
             <input
               type="text"
               {...register("userName", { required: "user name is required" })}
               placeholder="Name"
-              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
+              className="border-b-2 border-gray-500 px-1 ml-2 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-3 h-10"
             />
+
             {errors.userName && (
-              <p className="text-red-300">{errors.userName.message}</p>
+              <p className="text-red-300 w-full text-[10px] w-100 absolute top-15 ">
+                {errors.userName.message}
+              </p>
             )}
             <input
               type="text"
               {...register("lName", { required: "lastname is required" })}
               placeholder="Last name"
-              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
+              className="border-b-2 ml-2 border-gray-500 px-1 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-3 h-10"
             />
             {errors.lName && (
-              <p className="text-red-300">{errors.lName.message}</p>
+              <p className="text-red-300 text-[10px] w-100 absolute top-28">
+                {errors.lName.message}
+              </p>
             )}
           </div>
-          <div className="text-center">
+          <div className="text-center flex flex-col items-center relative">
             <input
               type="email"
               {...register("email", { required: "email is required" })}
               placeholder="email"
-              className="border-b-2 border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
+              className="border-b-2 border-gray-500 px-1 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-3 h-10"
             />
             {errors.email && (
-              <p className="text-red-300">{errors.email.message}</p>
+              <p className="text-red-300 text-sm text-[10px] w-100 absolute top-13 ">
+                {errors.email.message}
+              </p>
             )}
             <input
               type="number"
               {...register("Phonenumber", { required: "number is required" })}
               placeholder="Phone"
-              className="border-b-2  border-gray-500 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-5 h-10"
+              className="border-b-2 ml-2 border-gray-500 px-1 placeholder-[#4F4F4F] w-30 text-[13px] text-white mt-3 h-10"
             />
             {errors.Phonenumber && (
-              <p className="text-red-300">{errors.Phonenumber.message}</p>
+              <p className="text-red-300 text-[10px] w-100 absolute top-26">
+                {errors.Phonenumber.message}
+              </p>
             )}
           </div>
           <div className=" h-45 flex flex-col justify-center items-center">
@@ -81,14 +87,15 @@ export default function FormValidation() {
             ></textarea>
             <button
               type="submit"
+              text-sm
               className=" text-white bg-green-600 text-[12px] md:text-lg border-white border-1 mt-2 h-6 p-1"
             >
               Submit Here
             </button>
+            <br />
             {isSubmitSuccessful && (
-              <p className="text-green-300">successfully submited</p>
+              <p className="text-green-300  text-2xl">Successfully Submited</p>
             )}
-            {console.log(isSubmitSuccessful)}
           </div>
         </div>
       </form>
