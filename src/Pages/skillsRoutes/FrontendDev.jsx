7406308662f06d5c3469fea321f3206d8d89560c
Gurpreet <ajbagdi6@gmail.com>
2025-10-13 15:43:44 +0530

    updated frontend projects without live demo

--- src/Pages/skillsRoutes/FrontendDev.jsx
+++ src/Pages/skillsRoutes/FrontendDev.jsx
@@ -15,15 +15,15 @@ export default function FrontendDev() {
   console.log(frontendProjects);
   return (
     <>
-    <div className="md:flex flex-wrap">
-
-
-      {frontendProjects.map((items, index) => {
-        return (
-          <div key={index}   className="bg-[#1f1f1f] border-2 m-2 text-white rounded-2xl p-5 w-72 md:w-95 shadow-md
+      <div className="md:flex flex-wrap">
+        {frontendProjects.map((items, index) => {
+          return (
+            <div
+              key={index}
+              className="bg-[#1f1f1f] border-2 m-2 text-white rounded-2xl p-5 w-72 md:w-95 shadow-md
                  hover:shadow-green-400/40 transition-all duration-300 hover:scale-105
-                 flex  justify-between border border-[#333] hover:border-green-400">
-            
+                 flex  justify-between border border-[#333] hover:border-green-400"
+            >
               <div>
                 <h2 className="text-green-400 text-xl md:text-2xl font-semibold mb-2">
                   {items.title}
@@ -31,16 +31,18 @@ export default function FrontendDev() {
                 <p className="text-[#a1a1af] text-sm md:text-base mb-2">
                   {items.description}
                 </p>
-                <a href="https://github.com/Gurpreet-bit-maker/project_weather.git">Code Link</a>
+                <p className="text-gray-200 font-bold my-1">{items.tech.join(" - ")}</p>
+
+                <button className="border-2 px-1 rounded-lg hover:border-green-400 text-[10px]">
+                  <a href={items.codeLink}>Code Link</a>
+                </button>
                 <br />
                 <a href="">Live Demo</a>
               </div>
             </div>
-         
-        );
-      })}
-
-    </div>
+          );
+        })}
+      </div>
     </>
   );
 }
